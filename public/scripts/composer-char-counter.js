cd8ad5ccb10f6c7d24fdb0c9091124aefb375906
Benson <bensonwlee@gmail.com>
2023-07-18 19:03:43 -0400

    added character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,18 @@
+let i = 140
+
 $(document).ready(function () {
-  // --- our code goes here ---
+  const counter = $('.counter');
+  const tweetText = $("#tweet-text");
+
+  tweetText.on('input', function () {
+    const textLength = tweetText.val().length;
+    const remainingChars = i - textLength;
+    counter.text(remainingChars);
+
+    if (remainingChars < 0) {
+      counter.addClass('exceededLimit');
+    } else {
+      counter.removeClass('exceededLimit');
+    }
+  });
 });
