2d7780884b3bb5791d2eb2f318c9fa4b104d12b8
Benson <bensonwlee@gmail.com>
2023-07-19 18:47:08 -0400

    added dynamic tweets adjusted box shadow function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,71 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+// create tweet html structure for page 
+const createTweetElement = tweetData => {
+  const html = `
+<article>
+  <header class="tweet-header">
+  <div class="tweet-header-right">
+    <img style="margin: auto; background-color: transparent;" src="${tweetData.user.avatars}" alt="">
+    ${tweetData.user.name}
+  </div>
+  ${tweetData.user.handle}
+  </header>
+
+  <p>${tweetData.content.text}</p>
+
+  <footer class="tweet-footer">
+    ${tweetData.created_at}
+    <div class="tweet-icons">
+      <i id="flag" class="fa-solid fa-flag fa-2xs"></i>
+      <i id="retweet" class="fa-solid fa-retweet fa-2xs"></i>
+      <i id="heart" class="fa-solid fa-heart fa-2xs"></i>
+    </div>
+  </footer>
+  
+</article>
+`
+
+  return html
+};
+
+// add tweets to page
+const renderTweets = (tweetDataArray) => {
+  for (const tweetData of tweetDataArray) {
+    const tweet = createTweetElement(tweetData)
+    $("main").append(tweet)
+  }
+};
+
+// waits for document to fully load before running
+$(document).ready(() => {
+  renderTweets(tweetData)
+});
--- public/scripts/tweet-element.js
+++ public/scripts/tweet-element.js
@@ -1,39 +1,36 @@
+
 // Shadow box for tweet articles
 $(document).ready(function () {
-  const article = $("article");
-  article.hover(function () {
-    article.addClass('article-hover');
-  }, function () {
-    article.removeClass('article-hover');
+  $("article").on("mouseenter", event => {
+    $(event.currentTarget).addClass('article-hover');
+  }).on("mouseleave", event => {
+    $(event.currentTarget).removeClass('article-hover');
   });
 });
 
 // On hover icon colour flag
 $(document).ready(function () {
-  const flag = $("#flag");
-  flag.hover(function () {
-    flag.addClass('icons-hover');
-  }, function () {
-    flag.removeClass('icons-hover');
+  $("#flag").on("mouseenter", event => {
+    $(event.currentTarget).addClass('icons-hover');
+  }).on("mouseleave", event => {
+    $(event.currentTarget).removeClass('icons-hover');
   });
 });
 
 // On hover icon colour retweet
 $(document).ready(function () {
-  const retweet = $("#retweet");
-  retweet.hover(function () {
-    retweet.addClass('icons-hover');
-  }, function () {
-    retweet.removeClass('icons-hover');
+  $("#retweet").on("mouseenter", event => {
+    $(event.currentTarget).addClass('icons-hover');
+  }).on("mouseleave", event => {
+    $(event.currentTarget).removeClass('icons-hover');
   });
 });
 
 // On hover icon colour heart
 $(document).ready(function () {
-  const heart = $("#heart");
-  heart.hover(function () {
-    heart.addClass('icons-hover');
-  }, function () {
-    heart.removeClass('icons-hover');
+  $("#heart").on("mouseenter", event => {
+    $(event.currentTarget).addClass('icons-hover');
+  }).on("mouseleave", event => {
+    $(event.currentTarget).removeClass('icons-hover');
   });
 });
